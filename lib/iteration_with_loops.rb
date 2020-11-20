59d71a0e7493c9ebe85c38d3150d9ac1b9b43cb0
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-20 00:09:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def join_nested_strings(src)
   count=0
-  new_string=" "
+  new_string=""
   while count<src.length do
     inner_count=0
     while inner_count<src[count].length do
       if src[count][inner_count].class == String
-        new_string << src[count][inner_count]
+        new_string << src[count][inner_count] + " "
       end
     inner_count+=1
     end
